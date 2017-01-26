eadf828e8b4fe98f3408c755ce3568b4f8786e44
chelmakin119 <chelmakin119@gmail.com>
2017-01-26 18:34:22 +0500

    Adds function print in project

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let a = 54
+        var b = a
+        print(b)
     }
 
     override func didReceiveMemoryWarning() {
