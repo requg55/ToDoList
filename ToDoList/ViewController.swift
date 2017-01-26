82b513cf3e6727f469465d40faab754c80a18e93
chelmakin119 <chelmakin119@gmail.com>
2017-01-26 22:28:34 +0500

    Create UIAlert

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -1,28 +1,45 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var TblView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     //DataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return taskMrg.tasks.count;
+        return taskMrg.tasks.count //Количество строк в таблице
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell: UITableViewCell = UITableViewCell (style: UITableViewCellStyle.subtitle, reuseIdentifier: "Default")
-        cell.textLabel?.text = taskMrg.tasks[indexPath.row].name
-        cell.detailTextLabel?.text = taskMrg.tasks[indexPath.row].desc
+        cell.textLabel?.text = taskMrg.tasks[indexPath.row].name //Значение ячейки таблицы
         return cell
+    }
+    
+    //AddNewTask
+    
+    @IBAction func AddTasks(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "NewTask", message: "Please, input new task", preferredStyle: UIAlertControllerStyle.alert) //Создание контроллера UIAlert
+        alert.addTextField { (textField) in //Добавление текстового поля в UIAlert
+            textField.text = ""
+        }
+        //Добавление кнопок в UIAlert
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { [weak alert] (_) in
+            taskMrg.addTask(name: (alert?.textFields![0].text)!, desc: "ds")
+            alert?.textFields![0].text = ""
+            self.TblView.reloadData()//Обновление таблицы
+        }))
+        self.present(alert, animated: true, completion: nil)//Показать UIAlert
     }
 }
 
