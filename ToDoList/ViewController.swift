2ceff8baf988fd8fd1be5cc39436bab1a3c2f04d
chelmakin119 <chelmakin119@gmail.com>
2017-01-26 18:40:09 +0500

    Delete function print

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let a = 54
-        var b = a
-        print(b)
     }
 
     override func didReceiveMemoryWarning() {
