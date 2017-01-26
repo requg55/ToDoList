2fddd3b46e49d280bfb2c6c013e74b4f6d5df90b
chelmakin119 <chelmakin119@gmail.com>
2017-01-26 18:29:14 +0500

    Add first files

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Александр Челмакин on 26.01.17.
+//  Copyright © 2017 Александр Челмакин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
